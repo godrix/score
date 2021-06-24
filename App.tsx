53dfec1566df8a205b325bfb70bde200449c18a5
Carlos Godri <cggodri@gmail.com>
2021-06-24 01:06:29 -0300

    adicionado keepAwakr

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar'
 import { Home } from './src/Home';
+import { useKeepAwake } from 'expo-keep-awake';
+
 
 export default function App() {
+  useKeepAwake();
   return (
     <>
       <StatusBar style="light"/>
