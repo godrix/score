bbddbc1206714818b601a1219cdd2c477e745858
Carlos Godri <cggodri@gmail.com>
2021-06-24 01:05:01 -0300

    adicionado statusbar color

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
 import { Home } from './src/Home';
 
 export default function App() {
   return (
-    <Home/>
+    <>
+      <StatusBar style="light"/>
+      <Home/>
+    </>
   );
 }
